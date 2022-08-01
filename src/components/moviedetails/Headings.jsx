e088045fa11511cf2d7e1625d6ed2ec3bec84245
Swapnil Arun Meshram <swapnilm1908@gmail.com>
2022-08-02 03:22:03 +0530

    .

---
+++ src/components/moviedetails/Headings.jsx
@@ -0,0 +1,37 @@
+import { Box, Typography } from "@mui/material";
+import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
+import React from "react";
+
+export const Heading = ({headingString,arrow}) => {
+  console.log(arrow)
+  return (
+    <Box
+      variant="div"
+      sx={{
+        width: "100%",
+        display: "flex",
+        borderLeft: "4px solid rgb(245,197,24)",
+        alignItems: "center",
+        marginTop: "20px",
+        cursor: "pointer",
+        color: "rgb(33,33,33)",
+      }}
+    >
+      <Typography variant="h4" sx={{ fontSize:"30px",fontWeight: "700", paddingLeft: "5px" }}>
+        {headingString}
+      </Typography>
+      {
+        arrow?<ArrowForwardIosRoundedIcon
+        sx={{
+          fontSize: "30px",
+          marginLeft: "10px",
+          "&:hover": {
+            color: "rgb(245,197,24)",
+          },
+        }}
+      />:null
+      }
+      
+    </Box>
+  );
+};
--- src/components/moviedetails/MovieGenres.jsx
+++ src/components/moviedetails/MovieGenres.jsx
@@ -6,13 +6,13 @@ export const MovieGenres = ({genres}) => {
     <Typography
       variant="div"
       sx={{
-        color: "white",
+        color: "rgb(255,255,226)",
+        fontSize:"15px",
         borderRadius: "20px",
-        border: "2px solid white",
+        border: "2px solid rgb(174,174,174)",
         padding: "5px 20px",
         fontWeight: "500",
         cursor:"pointer",
-        marginLeft:"10px",
         "&:hover": {
           backgroundColor: "rgb(48,48,48)",
           borderColor: "#ffffff",
