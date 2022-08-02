dcea9a0b3b92d6a134286b3eeb5355de8fcdbc03
Jitender <58380090+jeetu9896@users.noreply.github.com>
2022-08-02 22:50:25 +0530

    Add Recent view bar

--- src/Components/CreateNewList.js
+++ src/Components/CreateNewList.js
@@ -1,20 +1,39 @@
-import { Box, Divider } from '@mui/material';
+import { Box, Divider, Stack, Button } from '@mui/material';
 import Form from './Form';
 const CreateNewList = () => {
     return <>
-        <Box sx= {{backgroundColor: "#e3e3dc", padding:"50px"}}>
-        <Box sx= {{backgroundColor: "#e3e3dc"}}>
-        <Box sx={{  height: "100%", display: "flex", flexDirection: "row", padding: "60px", margin:"0px 50px"}}>
-            <Box sx={{ backgroundColor: "white", width: "60%", textAlign: "left" }}  >
-                <Form />
+        <div>
+            <Box sx={{ backgroundColor: "#e3e3dc", padding: "50px" }}>
+                <Box sx={{ backgroundColor: "#e3e3dc" ,width:"1080px",margin:"auto"}}>
+                    {/* --------------------------------------- */}
+                    <Box sx={{ height: "100%", display: "flex", flexDirection: "row", width:"100%" }}>
+                {       /* -------------Form input---------------------------------------------- */}
+                        <Box sx={{ backgroundColor: "white", width: "70%", textAlign: "left" }}  >
+                            <Form />
+                        </Box>
+                        {/* ---------------------Divider Box----------------------------------------- */}
+                        <Box sx={{ backgroundColor: "#989899", width: "0.5%" }}></Box>
+                        {/* --------------------------Right Container------------------------------------------------------ */}
+                        <Box sx={{ backgroundColor: "#f1f0f1", width: "29.5%" }}>
+                            <p style={{ marginLeft: "10px", marginRight: "5px" }}><strong>Feedback?</strong> Tell us What you think about this Feature.</p>
+                        </Box>
+                    </Box>
+                    <Divider />
+                    {/* --------------------------------------Recent View---------------------------------------------------------------- */}
+                    <Box p={1} height={"200px"} sx={{backgroundColor: "#f1f0f1"}}>
+                        <Stack
+                            direction="row"
+                            justifyContent="space-between"
+                            alignItems="center"
+                            spacing={1}
+                        >
+                            <p style={{ fontSize: "18px", color: "#5a5a5a", margin: "0px 10px" }}>Recently Viewed</p>
+                            <Button variant="text" sx={{ fontSize: "10px", margin: "0px 10px" }}>Clear history</Button>
+                        </Stack>
+                    </Box>
+                </Box>
             </Box>
-            <Box sx={{ backgroundColor: "#989899", width: "0.2%" }}></Box>
-            <Box sx={{ backgroundColor: "#f1f0f1", width: "25%" }}>
-            <p style={{marginLeft:"10px",marginRight:"5px"}}><strong>Feedback?</strong> Tell us What you think about this Feature.</p>
-            </Box>
-        </Box>
-        </Box>
-        </Box>
+        </div>
     </>
 }
 export default CreateNewList;
