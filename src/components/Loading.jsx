b9f44c37feb3a01194a5dc7e65d154c86b6e4b54
Swapnil Arun Meshram <41813408+SwapnilMeshram19@users.noreply.github.com>
2022-08-03 16:27:36 +0530

    .

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import loading from "../loading_symbol.gif"
+export const Loading = () => {
+    return (
+
+    <div style={{width:"80%",height:"0",paddingBottom:"80%",position:"relative", margin:"auto", marginTop:"250px"}}>
+    <img src={loading} Width="80px"/>
+    </div>
+
+    )
+}
