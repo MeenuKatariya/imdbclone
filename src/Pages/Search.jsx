f7988e06ef23518e91ef1deb4d0a145c87a2f2ca
Meenu <meenukatariya2330@gmail.com>
2022-08-05 11:11:02 +0530

    .

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,36 +1,12 @@
 import React from 'react'
 import "./Search.css"
 export const  SearchBar=({prop})=> {
-    // console.log(prop)
-    const {Search}=prop;
-
-<<<<<<< HEAD
-    console.log(data,"data")
-=======
->>>>>>> 7263f93d9e93204676b80cb31aa464ed292ae944
-  return (
-    <>
-     {/* <p>{data.Title}</p> */}
-     {/* data.length==0?<div></div>:<div>
-     {
-     data.prop.map(e=>(
-
-          console.log(e)
-        
-      ))
-     }
-     </div> */}
-     
-
-<div>
   
-  {/* {
-    !data.Response?<div></div>:<div>
-        {
-            (data.Search).map((el)=>(<div>{console.log(el)}</div>))
-        }
-    </div>
-} */}
+    const {Search}=prop;
+  
+  return (
+    
+<div>
 
 {
   (Search==null)?<div></div>:<div>
@@ -43,7 +19,7 @@ export const  SearchBar=({prop})=> {
 }
 </div>
 
-    </>
+  
   )
 }
 
