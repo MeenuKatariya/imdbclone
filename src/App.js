12dbe4a4352f1ab854bd1408d4be22dc393c6c30
Jasmine Shaikh <jasmineshaikh54@gmail.com>
2022-08-06 02:03:33 +0530

    Merge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import CreateNewList from './CreateNewList';
+import "./App.css";
+import { Footer } from "./Components/component_jasmine/TempFooter";
+import { Navbar } from "./Components/component_jasmine/TempNavbar";
+import { Pages } from "./Pages/Pages";
 
 function App() {
   return (
-    <div className="App">
-    <CreateNewList />
+    <div>
+      <Navbar />
+      <Pages />
+      <Footer />
     </div>
   );
 }
